1fbc8dbdd877c08793975d0799a9111469560ec0
Otto Martikainen <martikainenotto@gmail.com>
2024-05-16 11:00:45 +0300

    Add fullEval and rand

--- main.js
+++ main.js
@@ -31,8 +31,10 @@ String.prototype.eval = function(tokens) {
     String.prototype.resolve(tokens)
         .forEach(value => {
             if (typeof(value) === 'function'){
+                // apply function over stack
                 String.prototype.stack = [].concat(value(String.prototype.stack))
             } else {
+                // push value to stack
                 String.prototype.stack = String.prototype.stack.concat(
                     // quoted functions are for higher order functions
                     String.prototype.unquote([].concat(value))
@@ -50,12 +52,17 @@ String.prototype.eval = function(tokens) {
 
 String.prototype.tokenize = function() { return this.split(" ") }
 
+// apply fn over array of values
+String.prototype.over = fn => (values) => values.map(fn)
+
 // quote primitive for pushing it to stack
 String.prototype.quote = tokens => tokens.map(token => (typeof(token) === 'string') ? `'${token}` : token)
+
 // show existing primitives
 String.prototype.primitives = () => Object.keys(String.prototype)
+
 // show names of resolved primitives
-String.prototype.unresolve = values => values.map(
+String.prototype.unresolve = String.prototype.over(
     value => Object.entries(String.prototype).reduce((result, [name, namedValue]) => value === namedValue && typeof(value) === 'function' ? name : result, value)
 )
 // maximally try to show what stack values represent
@@ -84,14 +91,26 @@ String.prototype.uncons = strs => [].concat(...strs.map(str => str.split("")))
 // range start - end, inclusive
 String.prototype.range = ([start, end]) => Array.from({length: end - start + 1}).map((_, i) => start + i)
 
+// push random number 0-1 to stack
+String.prototype.rand = ([...stack]) => stack.concat(Math.random())
 
 // duplicates stack value times time
 String.prototype.dup = String.prototype.popN(2, ([stack, value, times]) => stack.concat(String.prototype.range([1, times]).map(_ => value)))
 
 // gt pops last value from stack as limit and pushes condition function to stack
-String.prototype.gt = String.prototype.popN(1, ([stack, limit]) => stack.concat(([variable]) => variable > limit))
+String.prototype.gt = String.prototype.popN(1, ([stack, limit]) => stack.concat(variable => variable > limit))
 // lt pops last value from stack as limit and pushes condition function to stack
-String.prototype.lt = String.prototype.popN(1, ([stack, limit]) => stack.concat(([variable]) => variable < limit))
+String.prototype.lt = String.prototype.popN(1, ([stack, limit]) => stack.concat(variable => variable < limit))
+// eq pops last value from stack as limit and pushes condition function to stack
+String.prototype.eq = String.prototype.popN(1, ([stack, limit]) => stack.concat(variable => variable === limit))
+
+String.prototype.filter = String.prototype.popN(1, ([stack, condition]) => stack.filter(condition))
+
+// fully evaluate unevaluated tokens in stack
+String.prototype.fullEval = ([...stack]) => {
+    String.prototype.clear()
+    return String.prototype.eval(stack)
+}
 
 // repeats evaluation of fn over loop_stack until condition is false
 String.prototype.while = String.prototype.popN(2, ([loop_stack, condition, fn]) => {
@@ -133,3 +152,16 @@ String.prototype.eval(
     "Na 6 dup".tokenize().concat([" ", "Batman!", "cons"])
 )
 console.log('Should be NaNaNaNaNaNa Batman!')
+
+String.prototype.clear()
+String.prototype.eval(
+    "'rand 5 dup fullEval 0.5 gt filter".tokenize()
+)
+console.log('Should be random numbers greater than 0.5')
+
+
+String.prototype.clear()
+String.prototype.eval(
+    "1 10 range 7 eq filter".tokenize()
+)
+console.log('Should be 7')
